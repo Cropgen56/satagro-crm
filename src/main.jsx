b1189500bf61dc05194b616aa0e19af98d5bf08d
vishalpatilmaske <vishalpatilmaske@gmail.com>
2026-09-02 10:24:48 +0530

    update the

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { Provider } from 'react-redux'
-import { BrowserRouter } from 'react-router-dom'
-import AuthInitializer from '@/components/auth/AuthInitializer'
-import { store } from '@/store'
-import App from './App'
-import './index.css'
-import './styles/product-card-print.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import AuthInitializer from "@/components/auth/AuthInitializer";
+import { store } from "@/store";
+import App from "./App";
+import "./index.css";
+import "./styles/product-card-print.css";
 
-createRoot(document.getElementById('root')).render(
+/* eslint-disable react/jsx-no-constructed-context-values */
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
       <BrowserRouter>
@@ -18,4 +19,4 @@ createRoot(document.getElementById('root')).render(
       </BrowserRouter>
     </Provider>
   </StrictMode>,
-)
+);
